f2ffe7f1af7a58b9cfb58b0926d9862e8877f5d7
Andrey Golik <andrey@golik.us>
2019-10-21 22:29:53 -0400

    public move fix

--- util/copyStaticAssets.ts
+++ util/copyStaticAssets.ts
@@ -1,7 +1,7 @@
 import shell from 'shelljs';
 
 shell.cp('-R', 'src/views', 'dist/');
-shell.cp('-R', 'src/public/javascript/lib', 'dist/javascript/js/');
+shell.cp('-R', 'src/public/javascript/lib', 'dist/public/javascript/');
 shell.cp('-R', 'src/public/stylesheets/lib', 'dist/public/stylesheets/');
 shell.cp('-R', 'src/public/fonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
