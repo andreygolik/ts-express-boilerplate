d497802794e309b0c44e20d8125570727fd1a6b2
Andrey Golik <andrey@golik.us>
2019-10-21 22:24:24 -0400

    renamed public js and styles

--- util/copyStaticAssets.ts
+++ util/copyStaticAssets.ts
@@ -1,7 +1,7 @@
 import shell from 'shelljs';
 
 shell.cp('-R', 'src/views', 'dist/');
-shell.cp('-R', 'src/public/js/lib', 'dist/public/js/');
-shell.cp('-R', 'src/public/styles/lib', 'dist/public/styles/');
+shell.cp('-R', 'src/public/javascript/lib', 'dist/javascript/js/');
+shell.cp('-R', 'src/public/stylesheets/lib', 'dist/public/stylesheets/');
 shell.cp('-R', 'src/public/fonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
